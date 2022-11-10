f8c7fb0a8f97fa0539199e288d60674663123739
mitu-3216 <81210783+mitu-3216@users.noreply.github.com>
2022-11-10 22:11:15 +0600

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,20 @@ port_max = 65535
 open_ports = []
 
 while True:
-    ip_add_entered = st.write(input("\nPlease enter the ip address that you want to scan: "))
+    ip_add_entered = st.text_input("\nPlease enter the ip address that you want to scan: "))
+    if(st.button("Submit")):
     try:
         ip_address_obj = ipaddress.ip_address(ip_add_entered)
-        print("You entered a valid ip address.")
+        st.write("You entered a valid ip address.")
         break
     except:
-        print("You entered an invalid ip address")
+        st.write("You entered an invalid ip address")
 
 while True:
    
-    print("Please enter the range of ports you want to scan in format: (example would be 60-120)")
-    port_range =st.write( input("Enter port range: "))
+    st.write("Please enter the range of ports you want to scan in format: (example would be 60-120)")
+    port_range =st.write_input("Enter port range: ")
+    if(st.button("Submit")):
     port_range_valid = port_range_pattern.search(port_range.replace(" ",""))
     if port_range_valid:
         port_min = int(port_range_valid.group(1))
@@ -44,14 +46,14 @@ for port in range(port_min, port_max + 1):
         pass
 
 for port in open_ports:
-    print(f"Port {port} is open",", protocols is tcp")
+    st.write(f"Port {port} is open",", protocols is tcp")
     #print(socket.getservbyport({open_ports}))
     #print({port}'-v -sS -sV -sC -A -O')
     #print(socket.getservbyport(open_ports[ip_add_entered]))
     #print(socket.getservbyport(int(open_ports)))
     #print(scanner[ip_add_entered].all_protocols())
 
-    print("\n\n----------------Solution---------------\n\n")
+    st.write("\n\n----------------Solution---------------\n\n")
 
     print("1. Access ports using a secure virtual private network (VPn).\n2. Use multi-factor authentication.\n3. Implement network segmentation.\n4.Change Port number from standard to random unused one to help obfuscate. \n5.Setup firewall to restrict incoming traffic only to valid sources though up can be spoofed./n"
     "6.Blocking/restricting ports at the router \n7.configuring a firewall on the system the service is running on and blocking/restricting ports at the system - sometimes this firewall is part of an antivirus package \n8.Running monitoring software on your network that A) logs traffic for later analysis, B) updates and enacts IP and other blocklists from a service, and/or C) looks for patterns in incoming traffic and sends alerts if anything unusual is found.\n"
